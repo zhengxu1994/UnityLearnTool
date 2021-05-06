677e14f9926f368e192808fa13b1efa040ce8f09
zhengxu <zhengxu_1994@163.com>
2021-05-06 08:21:48 +0800

    101笔记

--- ToolProject/Assets/NewTool2021/Games101/Game101Note.cs
+++ ToolProject/Assets/NewTool2021/Games101/Game101Note.cs
@@ -117,6 +117,26 @@ p = (p·u)u + (p·v)v +(p·w)w
  *                (-sino  coso) 
  * 
  * 
+ * 三维矩阵
  * 
- * 
+ * Rx = (1, 0 ,   0 , 0)
+ *      (0,coso,-sino 0)
+ *      (0,sino,coso, 0)
+ *      (0, 0,   0,   1) 
+ *      
+ * Ry = (coso,   0,  sino , 0)
+ *      (0,      1,    0  , 0)
+ *      (-sino,  0,  coso , 0)
+ *      (0,      0,   0   , 1) 
+ *      
+ *      
+ * Rz = (coso,   -sino , 0  , 0)
+ *      (sino,   coso  , 0  , 0)
+ *      (0,        0,    1  , 0)
+ *      (0,        0,    0  , 1) 
+ *      
+ *      
+ *R(n,a) =                      T         (0  , -nz , ny)
+ *        cos(a)I + (1-cos(a))nn + sin(a) (nz ,  0  ,-nx)
+ *                                        (-ny,  nx , 0 )
  */
