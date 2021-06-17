058da01c124c06b8c41cceb1e66f91a4e399d910
zhengxu1994 <zhengxu_1994@163.com>
2021-06-17 16:02:05 +0800

    ilruntime Core

---
+++ Tool/ILRuntimeForCSHotfix-main/Core/Runtime/Stack/StackFrame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CSHotFix.CLR.Method;
+
+namespace CSHotFix.Runtime.Stack
+{
+    class IntegerReference
+    {
+        public int Value { get; set; }
+    }
+    unsafe struct StackFrame
+    {
+        public ILMethod Method;
+        public StackObject* LocalVarPointer;
+        public StackObject* BasePointer;
+        public StackObject* ValueTypeBasePointer;
+        public IntegerReference Address;
+        public int ManagedStackBase;
+    }
+}
